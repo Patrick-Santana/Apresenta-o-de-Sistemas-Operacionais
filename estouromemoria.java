fef43e3796c140297551c76ea57ef8a35310b2bf
Patrick-Santana <zonaabissal2016@gmail.com>
2019-12-19 07:51:38 -0300

    Create estouromemoria.java

---
+++ estouromemoria.java
@@ -0,0 +1,25 @@
+package estouromemoria;
+
+/**
+ *
+ * @author Patric
+ */
+public class EstouroMemoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int e = 999999999;
+        
+        int[] lista = new int[e];
+            
+        for (int i = 0; i < 999999999; i++){
+            lista[i] =  i;
+        } 
+        
+        
+    }
+    
+}
