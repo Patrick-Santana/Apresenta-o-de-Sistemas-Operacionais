a83c33f3c379bca130392665bbe0b0056add6ac1
Patrick-Santana <zonaabissal2016@gmail.com>
2019-12-19 08:23:55 -0300

    Create Gerenciamento.java

---
+++ Gerenciamento/Gerenciamento.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerenciamento;
+
+/**
+ *
+ * @author Patric
+ */
+public class Gerenciamento {
+    private int id;
+    private String nome;
+    private String usuario;
+    
+    public Gerenciamento(int id, String nome, String usuario){
+        this.id = id;
+        this.nome = nome;
+        this.usuario = usuario;
+    }   
+
+    protected void finalize(){
+            System.out.println("Arquivo de "+id+" foi removido");
+    }
+    
+    
+    
+    public boolean equals(Object o){
+        if(o instanceof Gerenciamento){
+            Gerenciamento t = (Gerenciamento)o;
+            return t.id == this.id && t.usuario.equals(this.usuario) && t.nome.equals(this.nome);
+        }
+        return false;
+    }
+    
+    public void mostrarInfo(){
+        System.out.println(id + " "+nome+" "+ usuario);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
